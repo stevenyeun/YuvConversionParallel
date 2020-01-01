80550d3d8eb255cbc69d3ef96a58d0615c3e5bb2
StevenYeun <mryeun2@gmail.com>
2020-01-01 21:39:13 +0900

    입고

---
+++ ParallelApp/ParallelColorConversion.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelApp
+{
+    unsafe class ParallelColorConversion
+    {
+        private static byte[] rgb;
+        private static int width;
+        private static int height;
+        private static int total;
+        private static ParallelOptions options = null;
+        public static void Init(int width, int height, int logicalCoreNum)
+        {   
+            ParallelColorConversion.width = width;
+            ParallelColorConversion.height = height;
+            total = width * height;
+            rgb = new byte[total * 3];
+            options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = logicalCoreNum;
+        }
+        public static byte[] ConvertYUV420toRGB888_Parallel(byte* yuv)
+        {
+            Parallel.For(0, height, options, (posY) =>
+            {
+                //Console.WriteLine("{0}: {1}",
+                //       Thread.CurrentThread.ManagedThreadId, posY);
+                for (int posX = 0; posX < width; posX++)
+                {
+                    //Parallel.For(0, width, (posX) =>
+                    //{
+                    int yPos = (posY * width + posX);
+                    byte y = yuv[yPos];
+                    byte u = yuv[(posY / 2) * (width / 2) + (posX / 2) + total];
+                    byte v = yuv[(posY / 2) * (width / 2) + (posX / 2) + total + (total / 4)];
+                    //byte u = 0;
+                    //byte v = 0;
+
+
+                    int rTmp = y + (int)(1.370705 * (v - 128));
+                    int gTmp = y - (int)((0.698001 * (v - 128)) - (0.337633 * (u - 128)));
+                    int bTmp = y + (int)(1.732446 * (u - 128));
+
+                    rgb[yPos * 3] = CLAMP(rTmp);
+                    rgb[yPos * 3 + 1] = CLAMP(gTmp);
+                    rgb[yPos * 3 + 2] = CLAMP(bTmp);
+                }
+                //});
+            });
+
+
+            return rgb;
+
+        }
+        public static byte[] ConvertYUV420toRGB888_Seq(byte* yuv)
+        {
+            for (int posY = 0; posY < height; posY++)
+            {
+                //Console.WriteLine("{0}: {1}",
+                //       Thread.CurrentThread.ManagedThreadId, posY);
+                for (int posX = 0; posX < width; posX++)
+                {
+                    //Parallel.For(0, width, (posX) =>
+                    //{
+                    int yPos = (posY * width + posX);
+                    byte y = yuv[yPos];
+                    byte u = yuv[(posY / 2) * (width / 2) + (posX / 2) + total];
+                    byte v = yuv[(posY / 2) * (width / 2) + (posX / 2) + total + (total / 4)];
+                    //byte u = 0;
+                    //byte v = 0;
+
+
+                    int rTmp = y + (int)(1.370705 * (v - 128));
+                    int gTmp = y - (int)((0.698001 * (v - 128)) - (0.337633 * (u - 128)));
+                    int bTmp = y + (int)(1.732446 * (u - 128));
+
+                    rgb[yPos * 3] = CLAMP(rTmp);
+                    rgb[yPos * 3 + 1] = CLAMP(gTmp);
+                    rgb[yPos * 3 + 2] = CLAMP(bTmp);
+                }
+                //});
+            }
+
+
+            return rgb;
+
+        }
+        static byte CLAMP(int x)
+        {
+            //((x > 255) ? 255 : (x < 0) ? 0 : x);
+
+            if (x > 255)
+            {
+                return 255;
+            }
+            else
+            {
+                if (x < 0)
+                    return 0;
+                else
+                    return (byte)x;
+            }
+        }
+    }
+}
---
+++ ParallelApp/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace ParallelApp
+{
+    unsafe class Program
+    {
+        static void Main(string[] args)
+        {
+#if false
+            // 1. 순차적 실행
+            // 동일쓰레드가 0~999 출력
+            //
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("{0}: {1}",
+                    Thread.CurrentThread.ManagedThreadId, i);
+            }
+            Console.Read();
+
+            // 2. 병렬 처리
+            // 다중쓰레드가 병렬로 출력
+            //
+            Parallel.For(0, 1000, (i) => {
+                Console.WriteLine("{0}: {1}",
+                    Thread.CurrentThread.ManagedThreadId, i);
+            });
+
+            Console.Read();
+#endif
+
+#if false
+            int width = 352;
+            int height = 288;
+            string file = "akiyo_cif.yuv";
+#endif
+            //int width = 960;
+            //int height = 540;
+            //int width = 720;
+            //int height = 576;
+
+            int width = 1920;
+            int height = 1080;
+            //string file = "D:\\yuv\\BigBuckBunny_1920_1080_24fps.420";
+            string file = "BigBuckBunny_1920_1080_24fps.420";
+
+
+            int ySize = width * height;
+            int uvSize = ySize / 2;
+            
+            //string file = "videoCam_960x540_420P.i420";
+            //string file = "720X576-YV12.yuv";
+            
+
+
+
+            int logicalCoreNum = Environment.ProcessorCount;
+            Console.WriteLine("core = " + logicalCoreNum);
+
+            File.SetAttributes(file, File.GetAttributes(file) | FileAttributes.ReadOnly);
+            using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                int totalSize = (int)fs.Length;
+                int totalNumOfFrame = totalSize / (ySize + uvSize);
+                int frameSize = ySize + uvSize;
+
+                byte[] buff = new byte[frameSize];
+                long totalTime = 0;
+
+                ParallelColorConversion.Init(width, height, logicalCoreNum);
+                for (int i = 0; i < totalNumOfFrame; i++)
+                {
+                    int n = fs.Read(buff, 0, frameSize);
+               
+                    fixed (byte* ptr = buff)
+                    {
+                        Stopwatch swh = new Stopwatch();
+                        swh.Start();
+
+                        byte[] RGB = ParallelColorConversion.ConvertYUV420toRGB888_Parallel(ptr);
+                        //byte[] RGB = ParallelColorConversion.ConvertYUV420toRGB888_Seq(ptr);
+
+                        swh.Stop();
+                        Console.WriteLine(swh.ElapsedMilliseconds.ToString()+"ms");
+                        totalTime += swh.ElapsedMilliseconds;
+#if false
+                        string fileName = String.Format("{0}.bmp", i + 1);
+                        Bitmap bitmap = CreateBitmap24bppRgb(RGB, width, height);
+                        bitmap.Save(fileName);
+#endif
+                        //Console.WriteLine(fileName);
+                    }
+                    Thread.Sleep(1000/15);
+                }
+                Console.WriteLine("avrProcTime=" + totalTime / totalNumOfFrame);
+            }
+
+
+            Thread.Sleep(5000);
+        }
+
+       
+
+        public static Bitmap CreateBitmap24bppRgb(byte[] bmpData, int width, int height)
+        {
+            if ((width * height * 3) != bmpData.Length)
+                throw new ArgumentException();
+
+            // Sometimes rows have an offset to complete a multiple of 4 number of bytes.  
+            // In that case:  
+            // int offset = 4 - ((width * 3) % 4);  
+            // if (offset == 4) offset = 0;  
+            // if (((width * 3 + offset) * height) != bmpData.Length)  
+            //     throw new ArgumentException();  
+
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            int pos = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    bmp.SetPixel(x, y, Color.FromArgb(bmpData[pos], bmpData[pos + 1], bmpData[pos + 2]));
+                    pos += 3;
+                }
+                //    pos += offset;  
+            }
+            return bmp;
+        }
+
+
+
+
+       
+
+
+   
+    }
+}
